6f4989058319dfba0de43a60ad8cc871f055289e
kevinwforney <kevinwilliamforney@gmail.com>
2022-11-04 13:50:05 +0800

    Initial Commit

---
+++ random/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  random
+//
+//  Created by User14 on 2022/11/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ random/randomApp.swift
@@ -0,0 +1,17 @@
+//
+//  randomApp.swift
+//  random
+//
+//  Created by User14 on 2022/11/4.
+//
+
+import SwiftUI
+
+@main
+struct randomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
